a9615389dd60a348f37ae417db8573bd4c34fa6a
Demian <komarovdemian@yandex.ru>
2023-07-03 00:29:53 +0300

    install script modified

--- installation.sh
+++ installation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install docker.io docker-compose python3-venv git tilix
+sudo apt install docker.io docker-compose python3-venv git npm
+
+git config --global user.name "Demian"
+git config --global user.email komarovdemian@yandex.ru
+
+sudo npm install yarn --global
+
+sudo snap install telegram-desktop
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install trello-desktop
